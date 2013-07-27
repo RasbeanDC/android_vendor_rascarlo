f3a3f234491340c777a2adc3175ca9f155cba072
RasbeanDC <kotwal961969@gmail.com>
2013-07-27 03:46:03 -0700

    Update and rename rascarlo_mako_en.mk to raskorr_mako_en.mk

--- products/raskorr_mako_en.mk
+++ products/raskorr_mako_en.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/rascarlo/products/common.mk)
+$(call inherit-product, vendor/raskorr/products/common.mk)
 
 # Setup device specific product configuration.
 PRODUCT_NAME := rascarlo_mako_en
@@ -17,7 +17,7 @@ PRODUCT_LOCALES := en_US en_GB
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.2.2 JDQ39 573038 release-keys"
 
 # Inherit media effect blobs
--include vendor/rascarlo/products/common_media_effects.mk
+-include vendor/raskorr/products/common_media_effects.mk
 
 # Inherit common build.prop overrides
--include vendor/rascarlo/products/common_versions.mk
+-include vendor/raskorr/products/common_versions.mk
