457a8382b1e35a5613d7fd429b5cb70f6bae761c
RasbeanDC <kotwal961969@gmail.com>
2013-07-27 03:49:12 -0700

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,10 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rascarlo_emulator.mk \
-    $(LOCAL_DIR)/rascarlo_grouper.mk \
-    $(LOCAL_DIR)/rascarlo_maguro.mk \
-    $(LOCAL_DIR)/rascarlo_maguro_en.mk \
-    $(LOCAL_DIR)/rascarlo_mako.mk \
-    $(LOCAL_DIR)/rascarlo_mako_en.mk \
-    $(LOCAL_DIR)/rascarlo_manta.mk \
-    $(LOCAL_DIR)/rascarlo_manta_en.mk \
-    $(LOCAL_DIR)/rascarlo_toro.mk
+    $(LOCAL_DIR)/raskorr_emulator.mk \
+    $(LOCAL_DIR)/raskorr_mako.mk \
+    $(LOCAL_DIR)/raskorr_mako_en.mk \
+    $(LOCAL_DIR)/raskorr_pyramid.mk
